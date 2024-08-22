9fc3dcecde9e52289e9cc866f9638f6509609ed1
lucas-faleiro <16035833+Lucas-Faleiro@users.noreply.github.com>
2024-08-22 12:22:19 -0300

    Add way for user to generate a new grid

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
+const containerArea = 480 * 480;
+const initialSquareSide = 24;
 
 const paintSquare = (e) => {
   e.target.style.backgroundColor = "black";
 };
 
-for (let i = 0; i < 16; i++) {
+const createNewGrid = () => {
+  const gridSize = Number(prompt("Please enter new size of grid", 16));
+  if (typeof gridSize !== "number" || gridSize > 40) {
+    alert("Please write a number lower or equal 40");
+    return "ERROR";
+  }
+  const numberOfSquares = gridSize * gridSize;
+  const sideOfSquare = Math.sqrt(containerArea / numberOfSquares) - 6; // minus 6 because of margin + border
+
+  container.innerHTML = "";
+  for (let i = 0; i < numberOfSquares; i++) {
+    const square = document.createElement("div");
+    square.setAttribute("class", "square");
+    square.style.width = sideOfSquare + "px";
+    square.style.height = sideOfSquare + "px";
+    square.addEventListener("mouseover", paintSquare);
+    container.appendChild(square);
+  }
+};
+
+//inital state
+for (let i = 0; i < 256; i++) {
   const square = document.createElement("div");
   square.setAttribute("class", "square");
+  square.style.width = initialSquareSide + "px";
+  square.style.height = initialSquareSide + "px";
   square.addEventListener("mouseover", paintSquare);
   container.appendChild(square);
 }
+
+button.addEventListener("click", createNewGrid);
