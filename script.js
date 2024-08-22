f295831f425e8358cc5695a7c68a6b25bc6c8fa9
lucas-faleiro <16035833+Lucas-Faleiro@users.noreply.github.com>
2024-08-22 05:27:23 -0300

    Change square color when user hover over it

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const container = document.querySelector(".container");
 
+const paintSquare = (e) => {
+  e.target.style.backgroundColor = "black";
+};
+
 for (let i = 0; i < 16; i++) {
   const square = document.createElement("div");
   square.setAttribute("class", "square");
+  square.addEventListener("mouseover", paintSquare);
   container.appendChild(square);
 }
